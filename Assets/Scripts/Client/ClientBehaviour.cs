82f1b1803577ea871929b279374c9799e0a175a5
william.scholder <william.scholder@edu.hefr.ch>
2022-04-16 14:41:40 +0200

    Update Client defining table to target.
    For multiple tables and multiple Clients.

--- Assets/Scripts/Client/ClientBehaviour.cs
+++ Assets/Scripts/Client/ClientBehaviour.cs
@@ -6,20 +6,48 @@ public class ClientBehaviour : MonoBehaviour
 {
     public float moveSpeed = 1f;
 
-    private GameObject table;
-
+    private GameObject[] tables;
     private Vector2 target;
+
+    private TableBehaviour targetedTable;
 
     // Start is called before the first frame update
     void Start()
     {
-        table = GameObject.Find("Table");
-        target = new Vector2(table.transform.position.x, table.transform.position.y);
+        tables = GameObject.FindGameObjectsWithTag("Table");
+        DefineTargetTable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        IsTableStillFree();
         transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+    }
+
+    private void IsTableStillFree()
+    {
+        //If the targetTable has client and it's not equals to himself then it should define a new table to target.
+        if (targetedTable.hasClient)
+        {
+            GameObject clientToTable = targetedTable.client;
+            if (clientToTable.name != this.name)
+            {
+                DefineTargetTable();
+            }
+        }
+    }
+
+    private void DefineTargetTable()
+    {
+        foreach (GameObject table in tables)
+        {
+            TableBehaviour tableScript = table.GetComponent<TableBehaviour>();
+            if (tableScript.hasClient == false)
+            {
+                targetedTable = table.GetComponent<TableBehaviour>();
+                target = new Vector2(table.transform.position.x - 1, table.transform.position.y);
+            }
+        }
     }
 }
